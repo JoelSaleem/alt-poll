d3246ad420fbf1414b0ff8e29540cca5f4c1fcae
JoelSaleem <joeljsaleem@gmail.com>
2020-12-30 15:26:31 +0000

    :sparkles: auth svc

--- auth/index.ts
+++ auth/index.ts
@@ -3,9 +3,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("ok");
+  res.send("hello world");
 });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log("auth listening on port 3000");
 });
