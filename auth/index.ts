11036298acb0668bf44a36f325be116a99295ca5
JoelSaleem <joeljsaleem@gmail.com>
2021-01-11 20:13:44 +0000

    :sparkles: create a helm chart and psql setup

--- auth/index.ts
+++ auth/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Client } from "pg";
 
 const app = express();
 
@@ -7,5 +8,28 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("auth listening on port 3000");
+  console.log("listenning on port 3000");
 });
+
+const client = new Client({
+  user: "postgres",
+  password: "foobar",
+  host: "alt-poll-auth.default.svc.cluster.local",
+  port: 5432,
+  database: "auth",
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log("connected");
+
+    return client.query('SELECT * FROM "Users" WHERE name = $1', ["Joel"]);
+  })
+  .then((results) => console.table(results.rows))
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    client.end();
+  });
