a4cfb90731991d0527c99003024b15b79ee2c6bd
JoelSaleem <joeljsaleem@gmail.com>
2021-02-15 20:49:38 +0000

    :sparkles: add Poll and Option events

--- common/src/messaging/events.ts
+++ common/src/messaging/events.ts
@@ -3,3 +3,15 @@ export enum UserEvents {
   USER_UPDATED = "user.updated",
   USER_DELETED = "user.deleted",
 }
+
+export enum PollEvents {
+  POLL_CREATED = "poll.created",
+  POLL_UPDATED = "poll.updated",
+  POLL_DELETED = "poll.deleted",
+}
+
+export enum OptionEvents {
+  OPTION_CREATED = "option.created",
+  OPTION_UPDATED = "option.updated",
+  OPTION_DELETED = "option.deleted",
+}
