a9a5b303f2cc3e0f559f5e187bae753e3a957943
JoelSaleem <joeljsaleem@gmail.com>
2021-06-12 15:58:38 +0100

    :sparkles: component-level state for PollForm

--- client/src/components/PollForm.tsx
+++ client/src/components/PollForm.tsx
@@ -1,27 +1,55 @@
+import { Input } from "@chakra-ui/input";
 import { Box, Center, Heading, Text } from "@chakra-ui/layout";
 import { Switch } from "@chakra-ui/switch";
 import { Textarea } from "@chakra-ui/textarea";
 import { PollDbProps } from "@js-alt-poll/common";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { Button } from "./Button";
 
 export const PollForm = ({ poll }: { poll: PollDbProps }) => {
   const { pathname, query, push } = useRouter();
-  const { title, open, closed, description } = poll;
+  const {
+    title: initialTitle,
+    open: initialOpen,
+    closed: initialClosed,
+    description: initialDescription,
+  } = poll;
+
+  const [title, setTitle] = useState(initialTitle);
+  const [open, setOpen] = useState(initialOpen);
+  const [closed, setClosed] = useState(initialClosed);
+  const [description, setDescription] = useState(initialDescription);
 
   return (
     <div>
       <Center>
-        <Heading>{title}</Heading>
+        <Heading>Poll</Heading>
       </Center>
+
       <Box>
-        Open: <Switch isChecked={open} />
+        title:
+        <Input value={title} onChange={(e) => setTitle(e.target.value)} />
       </Box>
+
       <Box>
-        Closed: <Switch isChecked={closed} />
+        Open:
+        <Switch isChecked={open} onChange={(e) => setOpen(e.target.checked)} />
       </Box>
+
+      <Box>
+        Closed:
+        <Switch
+          isChecked={closed}
+          onChange={(e) => setClosed(e.target.checked)}
+        />
+      </Box>
+
       <Text>Description</Text>
-      <Textarea value={description} />
+      <Textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
 
       <Center>
         <Button
@@ -36,7 +64,7 @@ export const PollForm = ({ poll }: { poll: PollDbProps }) => {
         >
           Back
         </Button>
-        <Button>Save</Button>
+        <Button onClick={() => {}}>Save</Button>
         <Button>Send voting link</Button>
       </Center>
     </div>
