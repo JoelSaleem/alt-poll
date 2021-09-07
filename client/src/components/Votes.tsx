fcb970dc605f40ab2bada592cc8a8c83e9e28088
JoelSaleem <joeljsaleem@gmail.com>
2021-09-07 20:51:54 +0100

    :sparkles: get options for otp client side

--- client/src/components/Votes.tsx
+++ client/src/components/Votes.tsx
@@ -1,3 +1,28 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+import { isLocalDev } from "../isLocalDev";
+
 export const Votes = () => {
+  const {
+    query: { otp },
+  } = useRouter();
+
+  if (!otp) {
+    return <div>Could not verify voting link</div>;
+  }
+
+  const { data } = useQuery(["votes", otp], async () => {
+    if (isLocalDev) {
+      return;
+    }
+
+    const res = await axios.get(`/api/votes/options/${otp}`);
+
+    return res.data;
+  });
+
+  console.log("%c data ", "background: purple; color: white");
+
   return <div>voting page</div>;
 };
