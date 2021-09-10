face88cec992e4202a0d36381a09c77ed53cd60f
JoelSaleem <joeljsaleem@gmail.com>
2021-09-10 17:29:41 +0100

    :sparkles: display options on votes screen

--- client/src/components/Votes.tsx
+++ client/src/components/Votes.tsx
@@ -1,7 +1,16 @@
+import { Container } from "@chakra-ui/layout";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useQuery } from "react-query";
 import { isLocalDev } from "../isLocalDev";
+import { Card } from "./Card";
+
+type Opt = {
+  otp_title: string;
+  opt_descr: string;
+  poll_title: string;
+  poll_descr: string;
+};
 
 export const Votes = () => {
   const {
@@ -12,17 +21,49 @@ export const Votes = () => {
     return <div>Could not verify voting link</div>;
   }
 
-  const { data } = useQuery(["votes", otp], async () => {
-    if (isLocalDev) {
-      return;
+  const { data, isLoading } = useQuery(
+    ["votes", otp],
+    async (): Promise<Opt[]> => {
+      if (isLocalDev) {
+        return [
+          {
+            otp_title: "abc",
+            opt_descr: "asdf",
+            poll_descr: "poll descr",
+            poll_title: "my poll",
+          },
+          {
+            otp_title: "vote for m3",
+            opt_descr: "asdfdasfasdf",
+            poll_descr: "poll descr",
+            poll_title: "my poll",
+          },
+        ];
+      }
+
+      const res = await axios.get(`/api/votes/options/${otp}`);
+
+      return res.data;
     }
+  );
 
-    const res = await axios.get(`/api/votes/options/${otp}`);
+  if (isLoading) return <div> Loading</div>;
 
-    return res.data;
-  });
+  const title = data && data.length > 0 ? data?.[0].poll_title : "unknown poll";
 
-  console.log("%c data ", "background: purple; color: white");
-
-  return <div>voting page</div>;
+  return (
+    <div>
+      voting page
+      {data?.map(({ opt_descr, otp_title, poll_descr, poll_title }) => {
+        return (
+          <Card depth={2} margin="2">
+            <Container padding="3">
+              <div>{otp_title}</div>
+              <div>{opt_descr}</div>
+            </Container>
+          </Card>
+        );
+      })}
+    </div>
+  );
 };
